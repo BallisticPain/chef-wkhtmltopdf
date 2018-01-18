f97cd268186be0bf5b186425945c12709edeb13e
Khaled Blah <kblah@avarteq.de>
2018-01-18 15:51:28 +0100

    Added support for tar.xz archive using tar CB

--- recipes/binary.rb
+++ recipes/binary.rb
@@ -1,5 +1,5 @@
 cache_dir = Chef::Config[:file_cache_path]
-download_dest = File.join(cache_dir, node['wkhtmltopdf-update']['package'])
+download_dest = File.join(cache_dir, node['wkhtmltopdf-update']['archive'])
 
 remote_file download_dest do
   source node['wkhtmltopdf-update']['mirror_url']
@@ -10,10 +10,24 @@ end
 package 'wkhtmltopdf' do
   source download_dest
   not_if "/usr/local/bin/wkhtmltopdf --version | grep #{node['wkhtmltopdf-update']['version']}"
+  not_if { download_dest.end_with?('tar.xz') }
+  checksum node['wkhtmltopdf-update']['archive_checksum'] if ['wkhtmltopdf-update']['archive_checksum']
 
   # if source.end_with?('.deb')
   #   provider Chef::Provider::Package::Dpkg
   # elsif source.end_with?('.rpm')
   #   provider Chef::Provider::Package::Rpm
   # end
+end
+
+tar_extract download_dest do
+  action :extract_local
+  target_dir Chef::Config[:file_cache_path]
+  creates "#{Chef::Config[:file_cache_path]}/wkhtmltox"
+  only_if { download_dest.end_with?('tar.xz') }
+end
+
+execute 'Copy wkhtmltox files' do
+  command "cp -r #{Chef::Config[:file_cache_path]}/wkhtmltox #{node['wkhtmltopdf-update']['root_dir']}"
+  only_if { ::File.exists("#{Chef::Config[:file_cache_path]}/wkhtmltox") }
 end
